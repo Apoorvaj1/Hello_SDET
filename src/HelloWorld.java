c93d76a039946e5a85b9def1f740614c994304f0
apoorv jain <apoorv127@gmail.com>
2023-12-09 17:28:49 +0530

    Fourth

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Enter any number");
         Scanner sc = new Scanner(System.in);
-        char convert3 =(char)sc.nextInt();
-        System.out.println(convert3);
+        char convert4 =(char)sc.nextInt();
+        System.out.println(convert4);
     }
 }
