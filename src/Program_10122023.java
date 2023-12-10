1302042d4e022174c8deaf0ecddd16c2681a62d0
apoorv jain <apoorv127@gmail.com>
2023-12-10 11:21:03 +0530

    Updated

--- src/Program_10122023.java
+++ src/Program_10122023.java
@@ -4,5 +4,8 @@ public class Program_10122023 {
     public static void main(String[] args){
         float a = 12.345f;
         System.out.println(a);
+
+        String name = "Apoorv";
+        System.out.printf("My name is %s", name);
     }
 }
